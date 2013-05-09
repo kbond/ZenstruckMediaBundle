a027777bc27ef73d71468a96a270220bcb82c9e7
Kevin Bond <kevinbond@gmail.com>
2013-05-09 11:07:12 -0400

    remove ROLE_MEDIA_ADMIN

--- Media/Permission/RolePermissionProvider.php
+++ Media/Permission/RolePermissionProvider.php
@@ -18,36 +18,31 @@ class RolePermissionProvider implements PermissionProviderInterface
 
     public function canMkDir()
     {
-        return $this->isGranted('ROLE_MEDIA_MKDIR');
+        return $this->securityContext->isGranted('ROLE_MEDIA_MKDIR');
     }
 
     public function canRenameFile()
     {
-        return $this->isGranted('ROLE_MEDIA_RENAME_FILE');
+        return $this->securityContext->isGranted('ROLE_MEDIA_RENAME_FILE');
     }
 
     public function canRenameDir()
     {
-        return $this->isGranted('ROLE_MEDIA_RENAME_DIR');
+        return $this->securityContext->isGranted('ROLE_MEDIA_RENAME_DIR');
     }
 
     public function canDeleteFile()
     {
-        return $this->isGranted('ROLE_MEDIA_DELETE_FILE');
+        return $this->securityContext->isGranted('ROLE_MEDIA_DELETE_FILE');
     }
 
     public function canDeleteDir()
     {
-        return $this->isGranted('ROLE_MEDIA_DELETE_DIR');
+        return $this->securityContext->isGranted('ROLE_MEDIA_DELETE_DIR');
     }
 
     public function canUploadFile()
     {
-        return $this->isGranted('ROLE_MEDIA_UPLOAD');
-    }
-
-    protected function isGranted($role)
-    {
-        return $this->securityContext->isGranted($role) || $this->securityContext->isGranted('ROLE_MEDIA_ADMIN');
+        return $this->securityContext->isGranted('ROLE_MEDIA_UPLOAD');
     }
 }
